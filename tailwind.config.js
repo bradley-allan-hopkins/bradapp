729ce3f3d3932e355bec476ebfacf43993f3405b
bradley-allan-hopkins <125572439+bradley-allan-hopkins@users.noreply.github.com>
2023-03-10 08:09:08 -0700

    Program able to sort and delete appointment. Removed manifest.json -> not needed Removed postcss.config.js -> not needed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  content: ["./src/**/*.{html,js}"],
+  //purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: ["./src/**/*.{ts,tsx,jsx,js}"],
   theme: {
     extend: {},
   },
-  plugins: [ require('@tailwindcss/forms')]
+  plugins: [],
 }
